1f46b79f19e6080d1d6d2366b7ea7eb7a81dc17d
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2024-04-10 16:35:11 -0400

    Comentamos datasource

--- src/main/java/py/com/tp1backend/config/DataSource.java
+++ src/main/java/py/com/tp1backend/config/DataSource.java
@@ -1,7 +1,7 @@
-package py.com.tp1backend.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DataSource {
-}
+//package py.com.tp1backend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DataSource {
+//}
